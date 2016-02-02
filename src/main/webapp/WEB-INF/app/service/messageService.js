47bc39febaccecf6f78d9346c7a4c8efb2d818ab
William Welling <wwelling@outlook.com>
2016-02-01 21:01:57 -0600

    Use AlertService in MessageService. mdToast doesn't position easily, if at all.

--- src/main/webapp/WEB-INF/app/service/messageService.js
+++ src/main/webapp/WEB-INF/app/service/messageService.js
@@ -1,29 +1,41 @@
-ascension.service('MessageService', [ '$mdToast', function($mdToast) {
+ascension.service('MessageService', [ '$mdToast', 'AlertService', function($mdToast, AlertService) {
 	
 	return {
-		info: function(message, delay) {			
-			$mdToast.show({
-				template: '<md-toast class="md-toast info">' + message + '</md-toast>',	        	
-				hideDelay: delay
-			});
+		info: function(message, delay) {
+			AlertService.add({ 'type': 'INFO', 
+							   'message': message 
+							 }, "foo/bar");
+//			$mdToast.show({
+//				template: '<md-toast class="md-toast info">' + message + '</md-toast>',	        	
+//				hideDelay: delay
+//			});
 		},
-		success: function(message, delay) {			
-			$mdToast.show({
-				template: '<md-toast class="md-toast success">' + message + '</md-toast>',
-	        	hideDelay: delay,
-			});
+		success: function(message, delay) {
+			AlertService.add({ 'type': 'SUCCESS', 
+							   'message': message 
+							 }, "foo/bar");
+//			$mdToast.show({
+//				template: '<md-toast class="md-toast success">' + message + '</md-toast>',
+//	        	hideDelay: delay,
+//			});
 		},
-		warning: function(message, delay) {			
-			$mdToast.show({
-				template: '<md-toast class="md-toast warning">' + message + '</md-toast>',
-	        	hideDelay: delay,
-			});
+		warning: function(message, delay) {
+			AlertService.add({ 'type': 'WARNING', 
+							   'message': message 
+							 }, "foo/bar");
+//			$mdToast.show({
+//				template: '<md-toast class="md-toast warning">' + message + '</md-toast>',
+//	        	hideDelay: delay,
+//			});
 		},
 		error: function(message, delay) {
-			$mdToast.show({
-				template: '<md-toast class="md-toast error">' + message + '</md-toast>',
-	        	hideDelay: delay,
-			});
+			AlertService.add({ 'type': 'ERROR', 
+							   'message': message 
+							 }, "foo/bar");
+//			$mdToast.show({
+//				template: '<md-toast class="md-toast error">' + message + '</md-toast>',
+//	        	hideDelay: delay,
+//			});
 		}
 	}
 	
